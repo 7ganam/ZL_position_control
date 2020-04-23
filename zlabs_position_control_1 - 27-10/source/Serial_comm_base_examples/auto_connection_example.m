1aa022210716c6e7b29239d75e4efaa99a3faf27
ganam <7ganam@gmail.com>
2020-04-23 17:19:50 +0200

    deleted unused files .. kept copy2 file as the main source codes

--- zlabs_position_control_1 - 27-10/source/Serial_comm_base_examples/auto_connection_example.m
+++
@@ -1,8 +0,0 @@
-delete(instrfindall);
-com_name = find_arduino_com_windowsOS();
-tic
-connection = serial(com_name,'BaudRate',112500);
-toc
-tic
-fopen(connection)
-toc
--- zlabs_position_control_1 - Copy/source/Serial_comm_base_examples/ML_to_Ard_with_lcd/send_data_no_recieve_example.m
+++
@@ -1,6 +0,0 @@
-delete(instrfindall);
-com_name = find_arduino_com_windowsOS();
-connection = serial(com_name,'BaudRate',112500);
-fopen(connection)
-pause(1) % withouth this the serial print dont have enough time to reach arduino
-fprintf(connection,'%s',"gains");
